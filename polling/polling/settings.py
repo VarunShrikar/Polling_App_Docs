1e5b83b38c351528c0189d9a33608d7ab3f477d6
varunshrikar <varunshrikar1995@gmail.com>
2022-07-25 08:00:37 +0530

    database config

--- polling/polling/settings.py
+++ polling/polling/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'polling.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'polls',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': 3306,
+        'HOST': '127.0.0.1'
     }
 }
 
@@ -105,9 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
