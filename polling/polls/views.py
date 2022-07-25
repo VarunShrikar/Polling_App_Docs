19ad8b5cc74ba71add9b20c40469c830bb1bee49
varunshrikar <varunshrikar1995@gmail.com>
2022-07-25 11:59:28 +0530

    detailed view created

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question, Choice
 
@@ -7,3 +9,11 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+# Specific Question with Choice
+def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/detail.html', { 'question': question })
